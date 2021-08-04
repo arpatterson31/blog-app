435febdc058252bf2fbcca48de6f2586595c4cab
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-04 13:42:48 -0700

    added a new reusable component for blog form

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+const BlogPostForm = () => {
+  return (
+    <Text>Blog Form</Text>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BlogPostForm;
