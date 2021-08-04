b73c5f24c50b5b2b751182148fedb947d154f8ef
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-04 11:22:06 -0700

    refactor to add a context creater to handle any new context and provider needed for app

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,29 +1,20 @@
-import React, { useReducer } from 'react';
-import { add } from 'react-native-reanimated';
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'add_blogpost':
-      return [...state, { title: `Blog Post # ${state.length + 1}`}];
-    default: 
+      return [...state, { title: `Blog Post # ${state.length + 1}` }];
+    default:
       return state;
   }
 }
 
-const BlogContext = React.createContext();
+const addBlogPost = () => {
+  dispatch({ type: 'add_blogpost' })
+}
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
-    dispatch({ type: 'add_blogpost' })
-  }
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
-};
-
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
