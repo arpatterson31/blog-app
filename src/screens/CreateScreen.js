7bd83b54cfc4e50c355da22ee159030d13848f8a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-04 13:12:17 -0700

    brought in addblog post from context and added that to onpress function on button

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,11 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const { addBlogPost} = useContext(Context);
 
   return (
     <View>
@@ -19,6 +20,10 @@ const CreateScreen = ({ navigation }) => {
         value={content}
         onChangeText={(text) => setContent(text)}
         style={styles.input}
+      />
+      <Button 
+        title="Add Blog Post"
+        onPress={() => addBlogPost(title, content)}
       />
     </View>
   );
