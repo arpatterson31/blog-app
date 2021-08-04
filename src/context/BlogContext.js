6c844165481ac879f9634070bc6dfc68717483f4
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-04 11:09:24 -0700

    switched from useState to useReducer to update state

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { add } from 'react-native-reanimated';
+
+const blogReducer = (state, action) => {
+  switch(action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post # ${state.length + 1}`}];
+    default: 
+      return state;
+  }
+}
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
-  };
+    dispatch({ type: 'add_blogpost' })
+  }
 
   return (
     <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
