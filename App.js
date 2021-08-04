1ec73a48297e5c6667ef8804c8e1e97b09a3eb0f
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-04 09:53:04 -0700

    dependencies install

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
