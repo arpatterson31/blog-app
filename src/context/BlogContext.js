7669ba5502f5b8fd5fc3e5d16fa9fea6edbd6571
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-04 10:55:39 -0700

    added state variable for blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { add } from 'react-native-reanimated';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [
-    { title: 'Blog Post #1' },
-    { title: 'Blog Post #2' },
-    { title: 'Blog Post #3' },
-  ]
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+  };
 
   return (
-    <BlogContext.Provider value={blogPosts}>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
     </BlogContext.Provider>
   );
